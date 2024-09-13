339a1cf958d096b84a954523f9f90327cf282578
treva556 <marktreva92@gmail.com>
2024-09-14 00:32:54 +0300

    milestone 1

--- app/page.js
+++ app/page.js
@@ -2,11 +2,37 @@
 
 export default function Home() {
   return (
-    <div className=" h-screen flex justify-center p-4 bg-yellow-500">
-      <div className=" bg-green-950 h-10 w-full flex justify-center text-white ">
-      NGO
-      </div>
-     
+    <>
+    <div className="h-screen w-full relative justify-around p-4 bg-yellow-500">
+  <div className="bg-green-950 h-20 m-4 flex justify-center items-center text-white">
+    NGO
+  </div>
+  <div className="bg-green-950 h-20 p-2 m-4 flex justify-center items-center text-white">
+    NGO
+  </div>
+  <div className=" h-20 p-2 m-4 mb-8  flex flex-row gap-10 justify-center items-center text-white relative z-10 -mt-10">
+  <div className=" bg-red-800 p-6 w-1/2">
+    NGO
     </div>
+    <div className=" bg-red-800 p-6 w-1/2">
+    NGO
+    </div>
+  </div>
+  <div className="bg-green-950 h-20 m-4 p-8 flex justify-center items-center text-white relative -mt-10">
+    NGO
+  </div>
+</div>
+<div className=" flex justify-around p-4 bg-yellow-500">
+  <div className=" bg-gray-800 p-8">
+kk
+  </div>
+  <div className=" bg-gray-500">
+kk
+  </div>
+</div>
+    
+    </>
+   
+
   );
 }
