41a8e8a81c07fde7f897bcbc54e1cbc060d7aef9
treva556 <marktreva92@gmail.com>
2024-09-15 03:14:30 +0300

    milestone 2

--- app/page.js
+++ app/page.js
@@ -3,26 +3,26 @@
 export default function Home() {
   return (
     <>
-    <div className="h-screen w-full relative justify-around p-4 bg-yellow-500">
-  <div className="bg-green-950 h-20 m-4 flex justify-center items-center text-white">
+    <div className="h-screen w-full relative justify-around p-4 bg-yellow-400">
+  <div className=" bg-green-900 h-80 m-4 flex justify-center items-center text-white">
     NGO
   </div>
-  <div className="bg-green-950 h-20 p-2 m-4 flex justify-center items-center text-white">
+  <div className=" bg-yellow-400 h-20 p-2 m-4 flex justify-center items-center text-white">
     NGO
   </div>
-  <div className=" h-20 p-2 m-4 mb-8  flex flex-row gap-10 justify-center items-center text-white relative z-10 -mt-10">
-  <div className=" bg-red-800 p-6 w-1/2">
+  <div className=" h-20 p-2 m-4 mb-8  flex flex-row gap-10 justify-center items-center relative z-10 -mt-10">
+  <div className=" bg-white p-6 w-1/2">
     NGO
     </div>
-    <div className=" bg-red-800 p-6 w-1/2">
+    <div className=" bg-white p-6 w-1/2">
     NGO
     </div>
   </div>
-  <div className="bg-green-950 h-20 m-4 p-8 flex justify-center items-center text-white relative -mt-10">
+  <div className="bg-green-900 h-20 m-4 p-8 flex justify-center items-center text-white relative -mt-10">
     NGO
   </div>
 </div>
-<div className=" flex justify-around p-4 bg-yellow-500">
+<div className=" flex justify-around p-4 bg-yellow-400">
   <div className=" bg-gray-800 p-8">
 kk
   </div>
